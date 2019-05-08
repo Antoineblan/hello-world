15a491da3bc4e9d8c1e44a881c298479ede7a198
Antoineblan <34807137+Antoineblan@users.noreply.github.com>
2019-05-08 21:23:13 +0200

    added missing indentation

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 
 def main():
-phrase = input("Tu veux dire quoi au monde entier?")
-print(phrase)
+  phrase = input("Tu veux dire quoi au monde entier?")
+  print(phrase)
 
 if __name__ == "__main__":
   main()
